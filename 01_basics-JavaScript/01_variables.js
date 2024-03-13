5c5c43b65b0e8a2ffdf3087eba8cda3a2f0b40e5
Vivek Kumar Gupta <80768248+vivek25800@users.noreply.github.com>
2024-03-13 15:53:40 +0000

    varaibles and datatype

---
+++ 01_basics-JavaScript/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 12356
+let acocuntEmail = "vivek@google.com"
+var accountPass = 456789
+accountCity = "Varanasi"
+let accountState
+
+// console.table([accountId, acocuntEmail, accountPass, accountCity])
+
+//accountId = 458978
+acocuntEmail = "vinay@hello.com"
+accountPass = 1234567
+accountCity = "Ballia"
+
+/*
+Prefer not use var because an issue in block scope and functional scope
+*/
+
+console.table([accountId, acocuntEmail, accountPass, accountCity, accountState])
---
+++ 01_basics-JavaScript/02_datatypes.js
@@ -0,0 +1,21 @@
+
+"use strict" // treat all JS code as newer version
+
+// Code readability should be high
+
+//Datatypes
+let name = "Vivek" //String datatype
+let age = 22 //Integer datatype
+let isLoggedIn = false //Boolean datatype
+
+// number => 2 to power 53 (range)
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof "vivek"); // string type
